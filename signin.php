9dbe9fcdffee5ba66be142482075268f544f9720
AbdelmalekBouguerra <abdelmalekbouguerra2000@gmail.com>
2022-05-06 18:28:22 +0100

    adding login php

--- signin.php
+++ signin.php
@@ -1,3 +1,4 @@
+<?php include 'src/login.php'?>
 <!doctype html>
 <html lang="en">
 
@@ -13,58 +14,37 @@
     <link rel="stylesheet" href="assets/animation/animate.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/responsive.css">
-    <title>KbDoc</title>
+    <title>Connexion - IPA</title>
 </head>
 
 <body data-scroll-animation="true">
-    <div id="preloader">
-        <div id="ctn-preloader" class="ctn-preloader">
-            <div class="round_spinner">
-                <div class="spinner"></div>
-                <div class="text">
-                    <img src="img/spinner_logo.png" alt="">
-                    <h4><span>KbDoc</span></h4>
-                </div>
-            </div>
-            <h2 class="head">Did You Know?</h2>
-            <p></p>
-        </div>
-    </div>
+    <?php include 'partials/preloader.html' ?> 
     <div class="body_wrapper">
         <section class="signup_area">
             <div class="row ml-0 mr-0">
                 <div class="sign_left signin_left">
-                    <h2>We are design changers do what matters.</h2>
+                    <h2>Accéder à votre compte pour mettre des tickets et bien d'autres fonctionnalités.</h2>
                     <img class="position-absolute top" src="img/signup/top_ornamate.png" alt="top">
-                    <img class="position-absolute bottom" src="img/signup/bottom_ornamate.png" alt="bottom">
-                    <img class="position-absolute middle" src="img/signup/door.png" alt="bottom">
-                    <div class="round"></div>
-                </div>
+                    <img class="position-absolute bottom" src="img/signup/bottom_ornamate.png" alt="bottom">                </div>
                 <div class="sign_right signup_right">
                     <div class="sign_inner signup_inner">
                         <div class="text-center">
-                            <h3>Sign in to KbDoc platform</h3>
-                            <p>Don’t have an account yet? <a href="signup.html">Sign up here</a></p>
-                            <a href="#" class="btn-google"><img src="img/signup/gmail.png" alt=""><span class="btn-text">Sign in with Gmail</span></a>
+                            <h3>Connectez-vous à la plateforme Help desk IPA</h3>
                         </div>
-                        <div class="divider">
-                            <span class="or-text">or</span>
-                        </div>
-                        <form action="#" class="row login_form">
+                        <form action="" methode = "POST" class="row login_form">
                             <div class="col-lg-12 form-group">
-                                <div class="small_text">Your email</div>
-                                <input type="email" class="form-control" id="email" placeholder="info@KbDoc.com">
+                                <div class="small_text">Votre Nom d'utilsateur</div>
+                                <input name="username" type="text" class="form-control" id="email" placeholder="username">
                             </div>
                             <div class="col-lg-12 form-group">
-                                <div class="small_text">Confirm password</div>
+                                <div class="small_text">Votre Mot de pass</div>
                                 <div class="confirm_password">
-                                    <input id="confirm-password" name="confirm-password" type="password" class="form-control" placeholder="5+ characters required" autocomplete="off">
-                                    <a href="#" class="forget_btn">Forgotten password?</a>
+                                    <input name="password" id="confirm-password" name="confirm-password" type="password" class="form-control" placeholder="mot de pass" autocomplete="off">
                                 </div>
                             </div>
 
                             <div class="col-lg-12 text-center">
-                                <button type="submit" class="btn action_btn thm_btn">Sign in</button>
+                                <button type="submit" class="btn action_btn thm_btn">CONNEXION</button>
                             </div>
                         </form>
                     </div>
