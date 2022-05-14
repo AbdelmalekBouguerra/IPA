361656bf11041b9ada0b1eb6cdcd56c6c0e3adaf
Assia Boutakhedmit <assiabouta2@gmail.com>
2022-05-14 10:00:06 +0100

    haref

--- home.php
+++ home.php
@@ -80,7 +80,7 @@ include 'src/ticket.php';
                             <!-- /.action-content -->
 
                             <div class="action-button-container">
-                                <a href="#" data-toggle="modal" data-target="#chose-type-ticket"
+                                <a href="C:\wamp64\www\IPA\model-ticket.html" 
                                     class="action_btn btn-ans">STAR</a>
                             </div>
                             <!-- /.action-button-container -->
@@ -336,8 +336,8 @@ include 'src/ticket.php';
             </div>
         </footer>
     </div>
-    <!-- including model of tickets -->
-    <?php include 'partials/model_ticket.html'; ?>
+    <!-- including model of tickets -->w
+    <?php include 'partials/model-ticket.html'; ?>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
